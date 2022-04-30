ed0f1cf507eb82538bebd4a5ecf678c1abb25cf3
Pete Stewart <pvstewar@gmail.com>
2022-04-29 21:57:56 -0400

    After adding login logic

--- app.py
+++ app.py
@@ -1,7 +1,67 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3 as sql
+from flask_login import login_required, current_user, login_user, logout_user
+from models import UserModel,db,login
 
 app = Flask(__name__)
+app.secret_key = 'r3JEzW1*mK%nuiMl7tSX'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ud.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+login.init_app(app)
+login.login_view = 'login'
+ 
+@app.before_first_request
+def create_all():
+    db.create_all()
+     
+@app.route('/welcome')
+@login_required
+def blog():
+    return render_template('welcome.html')
+ 
+ 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('/welcome')
+     
+    if request.method == 'POST':
+        email = request.form['email']
+        user = UserModel.query.filter_by(email = email).first()
+        if user is not None and user.check_password(request.form['password']):
+            login_user(user)
+            return redirect('/welcome')
+     
+    return render_template('login.html')
+ 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if current_user.is_authenticated:
+        return redirect('/blogs')
+     
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+ 
+        if UserModel.query.filter_by(email=email).first():
+            return ('Email already Present')
+             
+        user = UserModel(email=email, username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('register.html')
+ 
+ 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/welcome')
 
 @app.route('/')
 def hello():
